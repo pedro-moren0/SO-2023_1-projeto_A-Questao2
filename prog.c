81d70df3805dabf15525c964f14172d84cf4a4a5
pedro <pedro.moreno@aluno.ufabc.edu.br>
2023-03-14 20:59:16 -0300

    Added SIGINT and SIGTERM handlers

--- prog.c
+++ prog.c
@@ -3,17 +3,26 @@
 #include <stdlib.h> /* system call - exit */
 #include <unistd.h> /* system call - fork, exec, sleep */
 #include <stdio.h>
+#include <signal.h>
 
 #define true 1
 #define false 0
+
+void interruption_handler(int signum);
+void terminate_handler(int signum);
+void polite_exit_setup();
 
 // Variaveis globais
 int file1Open = true; /* Arquivo hipotetico 1 */
 int file2Open = true; /* Arquivo hipotetico 2 */
 int valor1 = 500;
+
 int main()
 {
 pid_t pid;
+    signal(SIGINT, interruption_handler);
+    signal(SIGTERM, terminate_handler);
+
     pid = fork();
     if (!pid)
     {
@@ -31,4 +40,46 @@ pid_t pid;
         printf(" O valor1 = %d\n",valor1);
         return 0;
     }
+}
+
+/*
+ * Handler function for the interruption signal.
+ *
+ * Params: The signal number
+ * Return: None
+ */
+void interruption_handler(int signum){
+    char response;
+    printf("\nDeseja realmente interromper o processo? ");
+    scanf("%c", &response);
+    
+    if(response == 'x' || response == 'X') {
+        polite_exit_setup();
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+ * Handler function for the terminate signal.
+ *
+ * Params: The signal number
+ * Return: None
+ */
+void terminate_handler(int signum){
+    polite_exit_setup();
+    exit(EXIT_FAILURE);
+}
+
+/*
+ * Routine to soft exit this program.
+ * Closes the open files and informs the value of valor1.
+ *
+ * Params: None
+ * Return: None
+ */
+void polite_exit_setup(){
+    file1Open = false;
+    file2Open = false;
+    printf("Closed all files.\n");
+    printf("Value of valor1 before closing = %d\n", valor1);
 }
